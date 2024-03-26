7a3743b84969b78f4c861f3145a184193388918b
atv2016 <alefveld@outlook.com>
2024-03-26 13:17:34 +0000

    add CANVAS_SIZE option

--- easy.py
+++ easy.py
@@ -5,6 +5,7 @@ import sys
 import easyocr
 import torch
 import gc
+import os
 
 # Do gc and memory cleanup as Pytorch is memory intensive
 gc.collect()
@@ -18,7 +19,17 @@ reader = easyocr.Reader(['en'],gpu=True)
 
 # Change parameters here. The larger the canvas size the more GPU memory will be used.
 
-result = reader.readtext(sys.argv[1], canvas_size=1000,detail=0)
+if "CANVAS_SIZE" in os.environ:
+    CANVAS_SIZE=int(os.environ["CANVAS_SIZE"])
+else:
+    CANVAS_SIZE=""
+
+if CANVAS_SIZE:
+    print(f"Using custom canvas size of { CANVAS_SIZE }")
+    result = reader.readtext(sys.argv[1],canvas_size=CANVAS_SIZE,detail=0)
+else:
+	print("Using default canvas size of 2560")
+	result = reader.readtext(sys.argv[1],detail=0)
 
 print(result)
 # Clean up reader object
