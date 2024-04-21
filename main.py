21e71e98d2b649fa735b08111d72f5aef877725c
Alef Veld <alefveld@outlook.com>
2024-04-21 10:30:55 +0100

    add IMAGE_SPLIT option
    IMAGE_SPLIT option left to right added to segment with ImageMagick

--- main.py
+++ main.py
@@ -26,18 +26,43 @@ app = Flask(__name__)
 def image(url):
     global reader
 
+    # Add ImageMagick options for specific use cases
+    if "IMAGE_SPLIT" in os.environ:
+        IMAGE_SPLIT=int(os.environ["IMAGE_SPLIT"])
+    else:
+        IMAGE_SPLIT=""    
+
+    if IMAGE_SPLIT:
+            print(f"Using convert to segment image",flush=True)
+            img_data= requests.get(url).content
+            with open('image.jpg','wb') as handler:
+                handler.write(img_data)
+            os.system('convert -crop 50%x100% image.jpg output.jpg')
+            url='output-0.jpg'
+            url_2='output-1.jpg'
+
+    else:
+            print(f"Using unmodified image for OCR",flush=True)
+
+
     # Works with images, how to make it work with url
     if "CANVAS_SIZE" in os.environ:
-	    CANVAS_SIZE=int(os.environ["CANVAS_SIZE"])
+            CANVAS_SIZE=int(os.environ["CANVAS_SIZE"])
     else:
         CANVAS_SIZE=""
 
     if CANVAS_SIZE:
-	    print(f"Using custom canvas size of { CANVAS_SIZE }")
-	    result = reader.readtext(url,canvas_size=CANVAS_SIZE,detail=0,paragraph=True)
+            print(f"Using custom canvas size of { CANVAS_SIZE }",flush=True)
+            result = reader.readtext(url,canvas_size=CANVAS_SIZE,detail=0,paragraph=True)
+            
+            if IMAGE_SPLIT:
+                result += reader.readtext(url_2,canvas_size=CANVAS_SIZE,detail=0,paragraph=True)
     else:
-	    print("Using default canvas size of 2560")
-	    result = reader.readtext(url,detail=0,paragraph=True)
+            print("Using default canvas size of 2560",flush=True)
+            result = reader.readtext(url,detail=0,paragraph=True)
+            
+            if IMAGE_SPLIT:
+                result += reader.readtext(url_2,detail=0,paragraph=True)
 
     #return jsonify(result)
 
